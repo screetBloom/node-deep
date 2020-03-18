7eef244cc970792a8601aa87f924d8d421aaa15a
screetBloom <15306225869@163.com>
2020-03-18 22:47:59 +0800

    测试webpack-chain

--- common-util/webpack-chain/index.js
+++ common-util/webpack-chain/index.js
@@ -1,8 +1,21 @@
 const Config = require('webpack-chain')
+const webpack = require('webpack')
+
+async function resolveWebpack() {
+  return new Promise((resolve, reject) => {
+    webpack(webpackConfig, (err, stats) => {
+      if (err) return reject(err)
+      if (stats.hasErrors()) return reject(`Build failed with errors.`)
+      resolve()
+    })
+  })
+}
 
 async function main() {
   const chainConfig = new Config()
   chainConfig.output.globalObject(`(typeof self !== 'undefined' ? self : this)`)
+  // 应用在项目中
+  await resolveWebpack()
 }
 
 main()
