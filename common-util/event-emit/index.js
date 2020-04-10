a01201a35c4b05261a372f00d457233d74133877
screetBloom <15306225869@163.com>
2020-04-10 21:54:21 +0800

    feat: event emit

--- common-util/event-emit/index.js
+++ common-util/event-emit/index.js
@@ -1,16 +1,16 @@
-
-const EventEmitter = require('events');
-class MyEmitter extends EventEmitter{};
-const myEmitter = new MyEmitter();
+const EventEmitter = require('events')
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter()
 
 myEmitter.on('event', function(data) {
-  console.log(data.num);  // 1
-  data.num++;
-});
-myEmitter.on('event', (data) => {
-  console.log(data.num); // 2
-});
+  console.log(data.num) // 1
+  data.num++
+})
+
+myEmitter.on('event', data => {
+  console.log(data.num) // 2
+})
 
 myEmitter.emit('event', {
   num: 1
-});
+})
