ae1c5288bee0d3ea6926ce74a9c936062f35c9fd
screetBloom <15306225869@163.com>
2020-06-03 11:27:29 +0800

    feat: 解析vue sfc自定义block

---
+++ common-util/sfc/index.js
@@ -0,0 +1,31 @@
+const vueTemplateCompiler = require('vue-template-compiler')
+const fs = require('fs-extra')
+
+function main() {
+  const vueContent = fs.readFileSync('./index.vue', 'utf8')
+  // console.log('\n')
+  // console.log(vueContent)
+  // console.log('\n')
+  const parsed = vueTemplateCompiler.parseComponent(vueContent, {
+    pad: 'space',
+  })
+  const routeBlock = parsed.customBlocks.find((b) => b.type === 'route')
+  let res = {}
+  if (routeBlock && routeBlock.content) {
+    try {
+      res = JSON.parse(JSON.stringify(routeBlock.content))
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  console.log(res)
+}
+
+function whatType(obj) {
+  return Object.prototype.toString
+    .call(obj)
+    .toLowerCase()
+    .replace(/(\[object\s)(\w+)(])/g, '$2')
+}
+
+main()
---
+++ common-util/sfc/index.vue
@@ -0,0 +1,21 @@
+<route>
+{
+  name: 'hhh',
+  cid: 123
+}
+</route>
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+body {
+  background-color: #f2f6fc;
+}
+</style>
